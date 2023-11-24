6a7538730db25c01b4361381c4b89ace558cfb25
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-11-24 19:29:19 -0300

    feat: ngFor lista

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.scss']
+  styleUrls: ['./listar-pensamento.component.scss'],
 })
 export class ListarPensamentoComponent {
-
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para componente filho',
+      autoria: 'Sou componente Pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Sou componente Filho',
+      modelo: 'modelo2',
+    },
+  ];
 }
