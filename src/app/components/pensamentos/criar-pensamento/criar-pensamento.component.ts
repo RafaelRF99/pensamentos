a5cba8b4949f2c0addc06f0449db69fd91edf51b
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-11-24 18:34:06 -0300

    feat: prop biding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.scss']
+  styleUrls: ['./criar-pensamento.component.scss'],
 })
 export class CriarPensamentoComponent {
-
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: '',
+  };
 }
