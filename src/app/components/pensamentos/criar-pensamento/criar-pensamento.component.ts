f66da1e50902e8bc3f9b8db38f288bf45f04512e
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-11-25 18:52:42 -0300

    feat: form reativo

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { IPensamento } from 'src/app/interface/IPensamento';
 import { PensamentoService } from '../pensamento.service';
 
@@ -9,21 +11,48 @@ import { PensamentoService } from '../pensamento.service';
   styleUrls: ['./criar-pensamento.component.scss'],
 })
 export class CriarPensamentoComponent {
-  pensamento: IPensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
+  formulario!: FormGroup;
 
-  constructor(private router: Router, private service: PensamentoService) {}
+  constructor(
+    private router: Router,
+    private service: PensamentoService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      autoria: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+      modelo: ['modelo1'],
+    });
+  }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe();
-    this.router.navigate(['/listarPensamento']);
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe();
+      this.router.navigate(['/listarPensamento']);
+    }
   }
 
   cancelar() {
     console.log('Ação cancelada!');
     this.router.navigate(['/listarPensamento']);
+  }
+
+  hablitarBotao() {
+    if (this.formulario.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
   }
 }
